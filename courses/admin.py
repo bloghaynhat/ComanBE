5728efbba5942a27621eb40a61525ff71610e65d
bloghaynhat <22683091.duc@student.iuh.edu.vn>
2025-04-24 07:43:49 +0700

    Add Event for admin

--- courses/admin.py
+++ courses/admin.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
-from .models import Course, Section, Lesson, Enrollment, LessonProgress
+from .models import Course, Section, Lesson, Enrollment, LessonProgress, Event, EventRegister
 
 admin.site.register(Course)
 admin.site.register(Section)
 admin.site.register(Lesson)
 admin.site.register(Enrollment)
 admin.site.register(LessonProgress)
+admin.site.register(Event)
+admin.site.register(EventRegister)
